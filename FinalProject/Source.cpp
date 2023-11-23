fe89252750665f4df567a626dff504f15c169b0b
Omar Vidaña Rodríguez <omviro@gmail.com>
2023-11-23 17:38:53 -0600

    Test Image added

--- FinalProject/Source.cpp
+++ FinalProject/Source.cpp
@@ -5,12 +5,15 @@
 int main() {
 
 	std::string winName = "Test";
-	cv::Mat source = cv::imread("TEST", cv::IMREAD_COLOR);
+	cv::Mat source = cv::imread("test.jpg", cv::IMREAD_COLOR);
 
-	//cv::Mat output = allTransformations(&source, 1, 1, 0, 0 , 90, 0, 0.5);
+	cv::Mat output = allTransformations(&source, 1, 1, 0, 0 , 90, 0, 0.5);
 
-	//cv::imshow(winName, output);
-	//cv::waitKey(0);
+	cv::imshow(winName, source);
+	cv::waitKey(0);
+
+	cv::imshow(winName, output);
+	cv::waitKey(0);
 
 	return 0;
 }
